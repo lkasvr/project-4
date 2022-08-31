893b0e6fccff2668a48f1b2a963ed3a087fd8a9b
Lucas Vieira <lkasvr@outlook.com>
2022-08-31 15:14:44 -0300

    Setting an solved problems of links with next/link and put correct navigate by the links in the web static pages

--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -1,8 +1,20 @@
 import P from 'prop-types';
+import Link from 'next/link';
 import * as Styled from './styles';
 
 export const MenuLink = ({ children, link, newTab = false }) => {
   const target = newTab ? '_blank' : '_self';
+  const nextLink = link.match(/^\//) ? true : false;
+
+  if (nextLink) {
+    return (
+      <Link href={link} passHref>
+        <Styled.Container href={link} target={target}>
+          {children}
+        </Styled.Container>
+      </Link>
+    );
+  }
 
   return (
     <Styled.Container href={link} target={target}>
