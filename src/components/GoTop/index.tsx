9f75e45838dd801086454be9c538383437474afc
Lucas Vieira <lkasvr@outlook.com>
2022-09-15 13:59:16 -0300

    test husky

--- src/components/GoTop/index.tsx
+++ src/components/GoTop/index.tsx
@@ -1,6 +1,8 @@
 import * as Styled from './styles';
 import { KeyboardArrowUp } from '@styled-icons/material-outlined';
 
+const a = '';
+
 export const GoTop = () => {
   return (
     <Styled.Container href="#" aria-label="Go to top" title="Go to top">
